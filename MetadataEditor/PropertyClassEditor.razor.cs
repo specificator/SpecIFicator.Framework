374dc650e8942de9f36b6b12023abe9592e78d67
Oliver Alt <oa@lacheweg21.de>
2022-11-15 18:17:11 +0100

    Extensions and improvements for TdSE 2022 demo (metadata edit)

--- MetadataEditor/PropertyClassEditor.razor.cs
+++ MetadataEditor/PropertyClassEditor.razor.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using MDD4All.SpecIF.ViewModels.Metadata;
+using MDD4All.SpecIF.DataModels;
+using MDD4All.SpecIF.DataModels.Manipulation;
 
 namespace SpecIFicator.Framework.MetadataEditor
 {
@@ -7,8 +9,34 @@ namespace SpecIFicator.Framework.MetadataEditor
     {
 
         [Parameter]
-        public PropertyClassesViewModel PropertyClassViewModel { get; set; }
+        public PropertyClassesViewModel DataContext { get; set; }
 
-        
+        protected override void OnInitialized()
+        {
+            if (DataContext.DataTypes.Any())
+            {
+                DataTypeViewModel firstElement = DataContext.DataTypes.First();
+
+                Key key = new Key();
+                key.InitailizeFromKeyString(firstElement.KeyString);
+
+
+                DataContext.PropertyClassUnderEdit.DataTypeKey = key;
+            }
+
+        }
+
+        private async Task OnTypeSelectionChanged(ChangeEventArgs args)
+        {
+            string selection = args.Value.ToString();
+            if (!string.IsNullOrEmpty(selection))
+            {
+                Key key = new Key();
+                key.InitailizeFromKeyString(selection);
+                DataContext.PropertyClassUnderEdit.DataTypeKey = key;
+
+
+            }
+        }
     }
 }
--- MetadataEditor/ResourceClassEditor.razor.cs
+++ MetadataEditor/ResourceClassEditor.razor.cs
@@ -1,3 +1,5 @@
+using MDD4All.SpecIF.DataModels;
+using MDD4All.SpecIF.DataModels.Manipulation;
 using MDD4All.SpecIF.ViewModels.Metadata;
 using Microsoft.AspNetCore.Components;
 
@@ -7,5 +9,28 @@ namespace SpecIFicator.Framework.MetadataEditor
     {
         [Parameter]
         public ResourceClassesViewModel DataContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (DataContext.PropertyClasses != null && DataContext.PropertyClasses.Any())
+            {
+                PropertyClassViewModel firstElement = DataContext.PropertyClasses[0];
+                Key key = new Key(firstElement.PropertyClass.ID, firstElement.PropertyClass.Revision);
+
+                DataContext.ResourceClassUnderEdit.SelectedPropertyClassToAdd = key;
+            }
+        }
+
+        private async Task OnPropertySelectionChanged(ChangeEventArgs args)
+        {
+            string selection = args.Value.ToString();
+            if (!string.IsNullOrEmpty(selection))
+            {
+                DataContext.ResourceClassUnderEdit.SelectedPropertyClassToAdd = new Key();
+                DataContext.ResourceClassUnderEdit.SelectedPropertyClassToAdd.InitailizeFromKeyString(selection);
+
+
+            }
+        }
     }
 }
