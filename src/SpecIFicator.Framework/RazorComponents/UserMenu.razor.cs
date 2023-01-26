7c27c3fa3789928105e8859bd01db0613fd5a431
Oliver Alt <oa@lacheweg21.de>
2023-01-26 11:39:41 +0100

    Test translations added for user menu

--- src/SpecIFicator.Framework/RazorComponents/UserMenu.razor.cs
+++ src/SpecIFicator.Framework/RazorComponents/UserMenu.razor.cs
@@ -1,26 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MDD4All.SpecIF.DataProvider.Contracts;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 namespace SpecIFicator.Framework.RazorComponents
 {
     public partial class UserMenu
     {
         [Inject]
+        private IStringLocalizer<UserMenu> L { get; set; }
+
+        [Inject]
         private ISpecIfDataProviderFactory SpecIfDataProviderFactory { get; set; }
 
         [Inject]
         private NavigationManager NavigationManager { get; set; }
 
-        private bool show = false;
+        private bool _show = false;
 
         private async Task OutFocus()
         {
             await Task.Delay(200);
-            this.show = false;
+            _show = false;
         }
 
         private void OnLogoutClick()
