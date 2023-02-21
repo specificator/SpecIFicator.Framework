56f410c7f38b3c9a7174adcf81d02fa05f304f43
Oliver Alt <oa@lacheweg21.de>
2023-02-21 12:30:58 +0100

    Add browser console logging extension for debugging

---
+++ src/SpecIFicator.Framework/LoggingExtensions/LoggingExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace SpecIFicator.Framework.LoggingExtensions
+{
+    public static class LoggingExtension
+    {
+        public static async void LogToWebConsoleAsync(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("console.log", message);
+        }
+    }
+}
--- src/SpecIFicator.Framework/RazorComponents/AddPluginStyles.razor.cs
+++ src/SpecIFicator.Framework/RazorComponents/AddPluginStyles.razor.cs
@@ -1,16 +1,35 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SpecIFicator.Framework.LoggingExtensions;
 using SpecIFicator.Framework.PluginManagement;
 
 namespace SpecIFicator.Framework.RazorComponents
 {
     public partial class AddPluginStyles
     {
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+
         private List<string> StylesheetNames { get; set; }
 
         protected override void OnInitialized()
         {
             StylesheetNames = PluginManager.GetStylesheetNames();
 
+            if(StylesheetNames != null )
+            {
+                JSRuntime.LogToWebConsoleAsync("StylesheetNames count = " + StylesheetNames.Count);
+                foreach(string stylesheetName in StylesheetNames)
+                {
+                    JSRuntime.LogToWebConsoleAsync($"{stylesheetName}");
+                }
 
+            }
+            else
+            {
+                JSRuntime.LogToWebConsoleAsync("StylesheetNames is null!");
+            }
+            
         }
     }
 }
