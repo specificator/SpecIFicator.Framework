d6eddcdfe64a654acdd2f667b58601f2348a9bea
Oliver Alt <oa@lacheweg21.de>
2024-10-30 14:33:15 +0100

    Add code for browser dimension service

---
+++ src/SpecIFicator.Framework/Services/BrowserDimensionService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using SpecIFicator.Framework.Services.DataModels;
+
+namespace SpecIFicator.Framework.Services
+{
+    public class BrowserDimensionService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public BrowserDimensionService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task<BrowserDimension> GetBrowserDimension()
+        {
+            return await _jsRuntime.InvokeAsync<BrowserDimension>("getDimensions");
+        }
+    }
+}
